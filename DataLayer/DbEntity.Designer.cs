16e15c76f6845f41c960c1ca1865dbf7be5bb55d
imrancs10 <imran.cs10@gmail.com>
2018-06-21 22:16:50 +0530

    EDMX chnaged

--- DataLayer/DbEntity.Designer.cs
+++ DataLayer/DbEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\MonitoringApp\PatientPortal\DataLayer\DbEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\sheikhimran\Desktop\Project\PatientPortal\PatientPotal\DataLayer\DbEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
