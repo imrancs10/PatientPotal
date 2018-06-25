17fe413e1a19cf576c85822367164935e32d3486
Satish Sonkar <btech.csit@gmail.com>
2018-06-25 23:33:00 +0530

    Deparment Script

---
+++ PatientPortal/Scripts/App/Global/App.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../jquery-1.10.2.js" />
+/// <reference path="App.js" />
+
+var app = {};
+
+app.urls = {};
+
+app.urls.departmentList = '/masters/GetDepartments';
+app.urls.departmentSave = '/masters/SaveDepartment';
+app.urls.departmentEdit = '/masters/EditDepartment';
+app.urls.departmentDelete = '/masters/DeleteDepartment';
