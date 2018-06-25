5677379913aba0fe472216d2645c09bc3ad6f4d9
Satish Sonkar <btech.csit@gmail.com>
2018-06-25 21:21:14 +0530

    DepartmentModel

---
+++ PatientPortal/Models/Masters/DepartmentModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientPortal.Models.Masters
+{
+    public class DepartmentModel
+    {
+        public int DepartmentId { get; set; }
+        public string DeparmentName { get; set; }
+    }
+}
