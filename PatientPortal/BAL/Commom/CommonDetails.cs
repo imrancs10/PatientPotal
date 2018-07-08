21302ca329e6e64f49ff7333fb2fcee287d79c4b
Satish Sonkar <btech.csit@gmail.com>
2018-07-08 20:54:03 +0530

    Doctor Schedule

---
+++ PatientPortal/BAL/Commom/CommonDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataLayer;
+using PatientPortal.Models.Common;
+
+namespace PatientPortal.BAL.Commom
+{
+    public class CommonDetails
+    {
+        PatientPortalEntities _db = null;
+
+        public List<DayModel> DaysList()
+        {
+            _db = new PatientPortalEntities();
+            var _list = (from day in _db.DayMasters
+                         select new DayModel
+                         {
+                             DayId = day.DayId,
+                             DayName = day.DayName
+                         }).ToList();
+            return _list != null ? _list : new List<DayModel>();
+        }
+    }
+}
