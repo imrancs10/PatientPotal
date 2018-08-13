833440d5a877bcf0d676ca2b137a3cca7cedca7e
Sheikh Imran <imran.cs10@gmail.com>
2018-08-13 22:25:02 +0530

    show eeror

--- PatientPortal/Infrastructure/Adapter/WebService/WebServiceIntegration.cs
+++ PatientPortal/Infrastructure/Adapter/WebService/WebServiceIntegration.cs
@@ -10,6 +10,8 @@ namespace PatientPortal.Infrastructure.Adapter.WebService
         {
             GetPatientDetails service = new GetPatientDetails();
             var result = service.getPatientDetails(crNumber);
+            if (result.ToLower().Contains("no record"))
+                return null;
             Serializer serilizer = new Serializer();
             result = result.Replace("<NewDataSet>", "").Replace("</NewDataSet>", "");
             return serilizer.Deserialize<HISPatientInfoModel>(result, "Table1");
