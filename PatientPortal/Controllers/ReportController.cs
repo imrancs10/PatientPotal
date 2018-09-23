be22888f64ee61716ccd71f39364d453f99aaf88
Satish Sonkar <btech.csit@gmail.com>
2018-09-23 23:03:58 +0530

    Billing Report

---
+++ PatientPortal/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientPortal.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult GetBillingReport()
+        {
+            return View();
+        }
+
+        public ActionResult ReportViewing()
+        {
+            return View();
+        }
+    }
+}
