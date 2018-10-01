404a311a25df10fd8dcca8ebf26d4bea4c37c212
Sheikh Imran <imran.cs10@gmail.com>
2018-10-01 11:30:05 +0530

    view report implementation

--- PatientPortal/Controllers/ReportController.cs
+++ PatientPortal/Controllers/ReportController.cs
@@ -18,7 +18,7 @@ namespace PatientPortal.Controllers
             ReportDetails _details = new ReportDetails();
             return View(_details.GetBillReportData());
         }
-        
+
         public ActionResult ReportViewing()
         {
             ReportDetails _details = new ReportDetails();
@@ -28,13 +28,13 @@ namespace PatientPortal.Controllers
         public ActionResult DownloadReportFile(string fileUrl)
         {
             string _fileDirectory = fileUrl.Substring(0, fileUrl.LastIndexOf("\\") + 1);
-            string _fileName= fileUrl.Substring(fileUrl.LastIndexOf("\\") + 1);
+            string _fileName = fileUrl.Substring(fileUrl.LastIndexOf("\\") + 1);
             if (Directory.Exists(_fileDirectory))
             {
                 string[] files = Directory.GetFiles(_fileDirectory);
                 if (files.Length > 0)
                 {
-                    var file = files.Where(x => x.Substring(x.LastIndexOf("\\")+1) == _fileName).FirstOrDefault();
+                    var file = files.Where(x => x.Substring(x.LastIndexOf("\\") + 1) == _fileName).FirstOrDefault();
                     if (file != null)
                     {
                         byte[] FileBytes = System.IO.File.ReadAllBytes(file);
@@ -44,6 +44,12 @@ namespace PatientPortal.Controllers
             }
 
             return RedirectToAction("GetBillingReport");
+        }
+        public FileResult GetReport(string url)
+        {
+            string ReportURL = @"D:\Personnal\PatientPortal\WorkingCode\PatientPotal\PatientPortal\\Reports\Bill\2018\November\2.pdf";
+            byte[] FileBytes = System.IO.File.ReadAllBytes(ReportURL);
+            return File(FileBytes, "application/pdf");
         }
     }
 }
