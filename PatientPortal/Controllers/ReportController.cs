897b6d677db06f80d934747518978a332c86470f
Sheikh Imran <imran.cs10@gmail.com>
2018-12-15 12:29:14 +0530

    report download and view work

--- PatientPortal/Controllers/ReportController.cs
+++ PatientPortal/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using PatientPortal.BAL.Reports;
+using PatientPortal.Global;
 using PatientPortal.Infrastructure.Utility;
 using System;
 using System.Collections.Generic;
@@ -105,19 +106,95 @@ namespace PatientPortal.Controllers
             return RedirectToRoute(fileUrl);
         }
 
-        public FileResult DownloadFile(string url)
+        public FileResult DownloadFile(string url, string ext)
         {
             WebClient client = new WebClient();
-            url = url.Replace("$", "/");
+            //url = CryptoEngine.Decrypt(url.Replace("*", "="));
             string downloadsPath = KnownFolders.GetPath(KnownFolder.Downloads);
-            client.DownloadFile(url, downloadsPath + "/" + "DownloadLabPdf.pdf");
+            client.DownloadFile(url, downloadsPath + "/" + "DownloadLabPdf." + ext);
 
-            byte[] fileBytes = System.IO.File.ReadAllBytes(downloadsPath + "/" + "DownloadLabPdf.pdf");
+            byte[] fileBytes = System.IO.File.ReadAllBytes(downloadsPath + "/" + "DownloadLabPdf." + ext);
             FileContentResult response = new FileContentResult(fileBytes, "application/octet-stream")
             {
-                FileDownloadName = "labreport" + DateTime.Now.Date + ".pdf"
+                FileDownloadName = "labreport" + DateTime.Now.Date + "." + ext
             };
             return response;
+        }
+        public ActionResult getpath(string path)
+        {
+            return Json(path, JsonRequestBehavior.AllowGet);
+        }
+        public FileStreamResult GetPDF(string path)
+        {
+            //Create a stream for the file
+            Stream stream = null;
+
+            //This controls how many bytes to read at a time and send to the client
+            int bytesToRead = 10000;
+
+            // Buffer to read bytes in chunk size specified above
+            byte[] buffer = new Byte[bytesToRead];
+
+            // The number of bytes read
+            try
+            {
+                //Create a WebRequest to get the file
+                HttpWebRequest fileReq = (HttpWebRequest)HttpWebRequest.Create(path);
+
+                //Create a response for this request
+                HttpWebResponse fileResp = (HttpWebResponse)fileReq.GetResponse();
+
+                if (fileReq.ContentLength > 0)
+                    fileResp.ContentLength = fileReq.ContentLength;
+
+                //Get the Stream returned from the response
+                stream = fileResp.GetResponseStream();
+
+                // prepare the response to the client. resp is the client Response
+                var resp = HttpContext.Response;
+
+                //Indicate the type of data being sent
+                resp.ContentType = "application/octet-stream";
+
+                //Name the file 
+                string fileName = "test.pdf";
+                resp.AddHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+                resp.AddHeader("Content-Length", fileResp.ContentLength.ToString());
+
+                int length;
+                do
+                {
+                    // Verify that the client is connected.
+                    if (resp.IsClientConnected)
+                    {
+                        // Read data into the buffer.
+                        length = stream.Read(buffer, 0, bytesToRead);
+
+                        // and write it out to the response's output stream
+                        resp.OutputStream.Write(buffer, 0, length);
+
+                        // Flush the data
+                        resp.Flush();
+
+                        //Clear the buffer
+                        buffer = new Byte[bytesToRead];
+                    }
+                    else
+                    {
+                        // cancel the download if client has disconnected
+                        length = -1;
+                    }
+                } while (length > 0); //Repeat until no data is read
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    //Close the input stream
+                    stream.Close();
+                }
+            }
+            return File(stream, "application/pdf");
         }
     }
 }
